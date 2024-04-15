1c0e7eb44128124b5159a8fd567d392ffaf0a6bc
Daniel Kiss <littlesnorrboy@gmail.com>
2024-04-15 09:42:40 +0200

    format

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const discord = require("discord.js")
+const discord = require("discord.js");
 const ghCore = require("@actions/core");
 const fs = require("fs");
 
@@ -17,25 +17,35 @@ function getActionEnv({ key, defaultValue = null, required = false }) {
     return defaultValue;
 }
 
-const client = new discord.Client({ intents: [discord.GatewayIntentBits.Guilds] });
-client.once(discord.Events.ClientReady, async c => {
-    const channel = await c.channels.fetch(getActionEnv({ key: "DISCORD_CHANNEL", required: true }));
-    const msgId = getActionEnv({ key: "DISCORD_MESSAGE", defaultValue: channel.lastMessageId });
-    const postFilePath = getActionEnv({ key: "POST_FILE", defaultValue: "/etc/discord-post/post" })
+const client = new discord.Client({
+    intents: [discord.GatewayIntentBits.Guilds],
+});
+client.once(discord.Events.ClientReady, async (c) => {
+    const channel = await c.channels.fetch(
+        getActionEnv({ key: "DISCORD_CHANNEL", required: true }),
+    );
+    const msgId = getActionEnv({
+        key: "DISCORD_MESSAGE",
+        defaultValue: channel.lastMessageId,
+    });
+    const postFilePath = getActionEnv({
+        key: "POST_FILE",
+        defaultValue: "/etc/discord-post/post",
+    });
 
     const data = fs.readFileSync(postFilePath);
     const messagePayload = data.toString();
 
     let body = { content: discord.escapeMarkdown(messagePayload) };
-    if (msgId && msgId != 'new') {
+    if (msgId && msgId != "new") {
         const msg = await channel.messages.fetch(msgId);
-        await msg.edit(body)
+        await msg.edit(body);
     } else {
-        await channel.send(body)
+        await channel.send(body);
     }
-    console.log("done")
+    console.log("done");
     process.exit(0);
 });
 
-const token = getActionEnv({ key: 'DISCORD_TOKEN', required: true });
+const token = getActionEnv({ key: "DISCORD_TOKEN", required: true });
 client.login(token);
